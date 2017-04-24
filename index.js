b6331a13cddcdb2f13951a68c5c00784ddb8878b
will saunders <montagist@gmail.com>
2017-04-23 22:26:38 -0400

    attempting to make irssi/ttystudio based recording command

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var irc = require('irc');
 var minimist = require('minimist');
+var childProcess = require('child_process');
+var Vantage = require('vantage');
 var compromise = require('compromise');
 var speakeasy = require('speakeasy-nlp');
 var convoSequence = require('./SeqStateMachine');
@@ -7,15 +9,19 @@ var convoSequence = require('./SeqStateMachine');
 const CMD_DELIM = "!";
 const CMD_REGEX = new RegExp( '^'+CMD_DELIM+'.*$' , 'i' );
 
-var ircConfig = { channels: ['#defcon201'],
+var ircConfig = { channels: ['#rBotsTest'],
 		  secure: true,
 		  selfSigned: true,
 		  certExpired: true,
-		  debug: true,
+		  debug: false,
 		  showErrors: true,
 		  port: 6697 };
 
-var client = new irc.Client( 'chat.freenode.net', 'adinodedc201', ircConfig );
+
+
+var client = new irc.Client( 'chat.freenode.net', 'adiTun201', ircConfig );
+
+process.theClient = client;
 
 var seqManager = new convoSequence.SeqStateMachine( [
 	convoSequence.lwrCaseMatchBuilder( "i am first match" ),
@@ -23,15 +29,72 @@ var seqManager = new convoSequence.SeqStateMachine( [
 	convoSequence.lwrCaseMatchBuilder( "i am third match" )
 ], function() { console.log( "fin." ); } );
 
+var vantage = new Vantage()
+			.delimiter("ani> ")
+			.listen(80)
+			.show();
+
+
+var commands = {
+
+	'!rec': function( meta, opts ) {
+	
+		var lbl = meta.user + (new Date()).getTime(),
+		    cmd = "./node_modules/ttystudio/bin/ttystudio";
+
+		console.log( "inside !rec method" );
+	
+		var ttyRecProc = childProcess.spawn( cmd, [ lbl + ".gif", "--log" ] );	// TODO: Sanitize 
+
+		ttyRecProc.stdout.on('data', function(data) {
+			console.log('stdout: ', data);
+		});
+
+		ttyRecProc.stderr.on('data', function(data) {
+			console.log('stderr: ', data);
+		});
+
+		ttyRecProc.on('close', function(code) {
+			console.log('child process exited with code ', code);
+		});
+
+		console.log( ttyRecProc );
+		ttyRecProc.stdin.write( "irssi -c chat.freenode.net --nick " + lbl + "\n" );
+
+		setTimeout( function() {
+
+			ttyRecProc.stdin.write( "\x11" );
+
+		}, 10000 );
+	}
+};
+
+
+
 client.addListener( "message", function ( from, to, message ) {
 
 	console.log( from + ' => ' + to + ': ' + message );
 
-	if ( message.match( CMD_REGEX ) )
-		console.log( minimist( message.split(" ") ) );
+	if ( message.match( CMD_REGEX ) ) {
+
+		var minOpts = minimist( message.toLowerCase().split(" ") );
+
+		console.log( minOpts['_'][0], !!commands[ minOpts['_'][ 0 ] ]  );
+
+		if ( commands[ minOpts['_'][ 0 ] ] ) {
+		
+			var theCmd = commands[ minOpts['_'][ 0 ] ] ;
+			theCmd( { 'user': from }, minOpts );
+		}
+	}
 
 	seqManager.executeSeq( from, to, message );
 
+} );
+
+client.addListener( "pm", function( from, msg ) {
+
+	console.log( from + ' => ' + msg );
 } );
 
 client.addListener( "error", function( msg ) {
