d19a06ab424c797f0790b41627a60794baad2779
will saunders <montagist@gmail.com>
2017-04-28 22:03:26 -0400

    moving logic to js file to be loaded as child process

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
 var irc = require('irc');
 var minimist = require('minimist');
 var childProcess = require('child_process');
-var Vantage = require('vantage');
-var compromise = require('compromise');
-var speakeasy = require('speakeasy-nlp');
 var convoSequence = require('./SeqStateMachine');
 
 const CMD_DELIM = "!";
 const CMD_REGEX = new RegExp( '^'+CMD_DELIM+'.*$' , 'i' );
-
-var ircConfig = { channels: ['#rBotsTest'],
-		  secure: true,
-		  selfSigned: true,
-		  certExpired: true,
-		  debug: false,
-		  showErrors: true,
-		  port: 6697 };
-
-
-
-var client = new irc.Client( 'chat.freenode.net', 'adiTun201', ircConfig );
-
-process.theClient = client;
 
 var seqManager = new convoSequence.SeqStateMachine( [
 	convoSequence.lwrCaseMatchBuilder( "i am first match" ),
 	convoSequence.lwrCaseMatchBuilder( "i am second match" ),
 	convoSequence.lwrCaseMatchBuilder( "i am third match" )
 ], function() { console.log( "fin." ); } );
-
-var vantage = new Vantage()
-			.delimiter("ani> ")
-			.listen(80)
-			.show();
 
 
 var commands = {
@@ -42,8 +20,6 @@ var commands = {
 		var lbl = meta.user + (new Date()).getTime(),
 		    cmd = "./node_modules/ttystudio/bin/ttystudio";
 
-		console.log( "inside !rec method" );
-	
 		var ttyRecProc = childProcess.spawn( cmd, [ lbl + ".gif", "--log" ] );	// TODO: Sanitize 
 
 		ttyRecProc.stdout.on('data', function(data) {
@@ -71,11 +47,15 @@ var commands = {
 
 
 
-client.addListener( "message", function ( from, to, message ) {
+process.on( "message", function ( msg ) {
 
-	console.log( from + ' => ' + to + ': ' + message );
+	var from = msg.from,
+	    to = msg.to,
+	    message = msg.message || "";
 
-	if ( message.match( CMD_REGEX ) ) {
+	console.log( msg, from + ' => ' + to + ': ' + message );
+
+	if ( message && message.match && message.match( CMD_REGEX ) ) {
 
 		var minOpts = minimist( message.toLowerCase().split(" ") );
 
@@ -90,14 +70,4 @@ client.addListener( "message", function ( from, to, message ) {
 
 	seqManager.executeSeq( from, to, message );
 
-} );
-
-client.addListener( "pm", function( from, msg ) {
-
-	console.log( from + ' => ' + msg );
-} );
-
-client.addListener( "error", function( msg ) {
-
-	console.error( msg );
 } );
