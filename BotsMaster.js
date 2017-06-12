06dab72eb2b9fd0fac94cabfd9e11b7fe3691935
will saunders <montagist@gmail.com>
2017-06-12 00:29:13 -0400

    moving connection logic into...connectors. first inklings of overall bot orchestrator class

--- BotsMaster.js
+++ BotsMaster.js
@@ -1,98 +1,54 @@
-var irc = require('irc');
 var forkie = require('forkie');
-var colors = require('colors/safe');
 
 var masterProc = require('./patched_master')( [ "botfile.js" ], { repl: {
     name: 'master-repl',
     path: process.cwd()
   } } );
 
-masterProc.on('worker stopped', function(metas) {
+var IRCConnector = require( './serviceConnectors/IRC' );
+var ircClient = new IRCConnector( { channels: ['#rBotsTest'] } );
 
-	console.log(metas.title); // worker title, see worker API 
-	console.log(metas.code)   // exit code 
-	console.log(metas.signal) // exit signal, should be SIGKILL when killTimeout occurs 
-});
- 
-var client,
-    ircConfig = { channels: ['#rBotsTest'],
-                  secure: true,
-                  selfSigned: true,
-                  certExpired: true,
-                  debug: false,
-                  showErrors: true,
-                  port: 6697 };
+var SlackConnector = require( './serviceConnectors/Slack' );
+var slackClient = new SlackConnector();
 
-// on ready and started events, you get the `{ title: 'worker title' }` 
-masterProc.on('worker ready', console.log );
+// TODO: iterate over passed in Connectors
+ircClient.init( function( msg ) {
+
+	for ( var fi = 0; fi < masterProc.forks.length; fi++ )
+		masterProc.forks[fi].send( msg );
+} );
+
+slackClient.init( function( msg ) {
+
+	for ( var fi = 0; fi < masterProc.forks.length; fi++ )
+		masterProc.forks[fi].send( msg );
+} );
+
 masterProc.on('worker started', function() {
 
+	var msgHandler = function( msg ) {  
+
+		switch ( msg.type ) {
+
+			case "chat":
+				ircClient.say( msg.to, msg.msg );	
+			break;
+		
+			case "pm":
+				ircClient.pm( msg.to, msg.msg );
+			break;
+		}		
+	};
 
         for ( var fi = 0; fi < masterProc.forks.length; fi++ ) {       
 
-                masterProc.forks[fi].on( 'message', function( msg ) {
-
-			console.log( "master msg", msg );
-
-			if ( msg.type && msg.type === "chat" ) {
-
-				// Do I like this checking of the chat service?
-				var destEntity;
-
-				if ( msg.to.indexOf( ":" ) !== -1 ) {
-
-					var pieces = msg.to.split( ":" ),
-					    destService = pieces[ 0 ],
-					    destEntity = pieces[ 1 ];
-
-					// TODO: will use destService as a key to get
-					// reference to whatever instantiated client
-					// for that particular chat service
-					
-				} else {
-
-					destEntity = msg.to;
-				}
-
-				client.say( destEntity, msg.msg );	
-			}
-		} );
+		// TODO: regex keying against handlers
+                masterProc.forks[fi].on( 'message', msgHandler );
 	}
 } );
 
-client = new irc.Client( 'chat.freenode.net', 'adiTun201', ircConfig );
 
-client.addListener( "message", function ( from, to, message ) {
+ircClient.client.addListener( "error", function( msg ) {
 
-	console.log( from + ' => ' + to + ': ' + message );
-
-	var theMsg = { from: from,
-			to: to,
-			msg: message,
-			type: "chat" };
-
-	for ( var fi = 0; fi < masterProc.forks.length; fi++ )
-		masterProc.forks[fi].send( theMsg );
+	console.error( msg );
 } );
-
-client.addListener( "pm", function( from, msg ) {
-
-	console.log( from + ' => ' + msg );
-} );
-
-client.addListener( "error", function( msg ) {
-
-	console.error( colors.red( msg ) );
-} );
- 
-function startMaster(cb) {
-
-	setTimeout(cb, 3000);
-}
- 
-function stopMaster(cb) {
-
-	setTimeout(cb, 1500);
-}
-
-
