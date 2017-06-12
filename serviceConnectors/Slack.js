2e6937249ff1d64c3b25ca0627edef1d70e10bf5
will saunders <montagist@gmail.com>
2017-06-12 02:16:19 -0400

    mapping out channel ids

--- serviceConnectors/Slack.js
+++ serviceConnectors/Slack.js
@@ -14,19 +14,29 @@ function SlackConnector( opts ) {
 	this.service = "slack";
 	this.client = null;
 	this.opts = _.extend( defConfig, opts );
+	this.channel2Id = {};
 
 	this.init = function( msgProcessSlot ) {
 	
 		this.client = new slack.RtmClient( this.opts.token );
 
-		var conn = this;
-
-		this.client.on( slack.RTM_EVENTS.MESSAGE, function ( msg ) {
-
+		var conn = this,
+		    msgHand = function( msg ) {
+			
 			console.log( "slack msg: ", msg );
 
 			msgProcessSlot( genMsg( msg.user, conn.opts.nick, msg.text, "chat" ) );
+		};		
+
+		this.client.on( slack.CLIENT_EVENTS.RTM.AUTHENTICATED, function( rtmStartData ) {
+
+			rtmStartData.channels.map( function( c ) {
+				conn.channel2Id[ c.name ] = channel = c.id;
+  			} );
 		} );
+
+		this.client.on( slack.RTM_EVENTS.MESSAGE, msgHand );
+		this.client.on( slack.RTM_EVENTS.IM_CREATED, msgHand );
 
 		this.client.start();
 
@@ -53,7 +63,8 @@ function SlackConnector( opts ) {
 
 	this.say = function( to, msg ) {
 
-		this.client.sendMessage( msg, to );
+		var channelId = this.channel2Id[ to.toLowerCase() ];
+		this.client.sendMessage( msg, channelId );
 	};
 
 	this.pm = this.say;
