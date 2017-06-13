2f6ea4452063fa3424860365b53ef496724a61fe
will saunders <montagist@gmail.com>
2017-06-13 19:03:28 -0400

    Factorizing chat service type based on incoming msgs from workers. Made BotsMaster logic more class and functional.

--- BotsMaster.js
+++ BotsMaster.js
@@ -1,54 +1,75 @@
 var forkie = require('forkie');
-
-var masterProc = require('./patched_master')( [ "botfile.js" ], { repl: {
-    name: 'master-repl',
-    path: process.cwd()
-  } } );
+var Master = require('./patched_master.js');
 
 var IRCConnector = require( './serviceConnectors/IRC' );
-var ircClient = new IRCConnector( { channels: ['#rBotsTest'] } );
+var ircClient = new IRCConnector( { channels: ['#rBotsTest'],
+				    nick: "adibot" } );
 
 var SlackConnector = require( './serviceConnectors/Slack' );
 var slackClient = new SlackConnector();
 
-// TODO: iterate over passed in Connectors
-ircClient.init( function( msg ) {
+var connectors = [ ircClient, slackClient ],
+    botfiles = [ "botfile.js" ];
 
-	for ( var fi = 0; fi < masterProc.forks.length; fi++ )
-		masterProc.forks[fi].send( msg );
-} );
+function BotsMaster( connectors, botfiles ) {
 
-slackClient.init( function( msg ) {
+	var scope = this;
 
-	for ( var fi = 0; fi < masterProc.forks.length; fi++ )
-		masterProc.forks[fi].send( msg );
-} );
+	this.masterProc = Master( botfiles, 
+				  { repl: { name: 'master-repl',
+				    	    path: process.cwd() } } );
 
-masterProc.on('worker started', function() {
+	var msgPass2Workers = function( msg ) {
 
-	var msgHandler = function( msg ) {  
-
-		switch ( msg.type ) {
-
-			case "chat":
-				ircClient.say( msg.to, msg.msg );	
-			break;
-		
-			case "pm":
-				ircClient.pm( msg.to, msg.msg );
-			break;
-		}		
+		scope.masterProc.forks.map( function( fork ) {
+			// TODO: regex keying against workers
+			forks.send( msg );
+		} );
 	};
 
-        for ( var fi = 0; fi < masterProc.forks.length; fi++ ) {       
+	this.service2Connector = {};
 
-		// TODO: regex keying against handlers
-                masterProc.forks[fi].on( 'message', msgHandler );
-	}
-} );
+	connectors.map( function( conn ) {
 
+		// allows cross-service via simple msg-dispatching
+		scope.service2Connector[ conn.service ] = conn;
 
-ircClient.client.addListener( "error", function( msg ) {
+		// service connection & routing msgs to individual workers 
+		conn.init( msgPass2Workers );
+	} ); 
 
-	console.error( msg );
-} );
+	this.masterProc.on('worker started', function() {
+
+		var msgFromWorkerHandler = function( msg ) {  
+
+			// matching Connector by requested msg service
+			var theClient = scope.service2Connector[ msg.serv ];
+
+			if ( !theClient ) {
+
+				console.error( "Connector not found for service: " + msg.serv );
+				return;
+			}
+
+			switch ( msg.type ) {
+
+				case "chat":
+					theClient.say( msg.to, msg.msg );	
+				break;
+			
+				case "pm":
+					theClient.pm( msg.to, msg.msg );
+				break;
+			}		
+		};
+
+		for ( var fi = 0; fi < scope.masterProc.forks.length; fi++ ) {       
+
+			scope.masterProc.forks[fi].on( 'message', msgFromWorkerHandler );
+		}
+	} );
+}
+
+var bm = new BotsMaster( connectors, botfiles );
+
+module.exports = BotsMaster;
