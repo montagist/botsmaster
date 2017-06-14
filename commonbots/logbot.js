ba8f5ea54f6ef214d3f661a2fbf7f9520c00dd97
will saunders <montagist@gmail.com>
2017-06-13 22:08:18 -0400

    first attempt at logic someone will actually use. logs chats to a file

---
+++ commonbots/logbot.js
@@ -0,0 +1,23 @@
+var forkie = require('forkie');
+var fs = require('fs');
+var util = require('util');
+
+const BOT_TITLE = "LogBot";
+const LOGFILE = "chats.log";
+var workOpts = { start: function(c){ setTimeout( c, 3000 ); },
+		 stop:  function(c){ setTimeout( c, 1500 ); } };
+
+var worker = forkie.worker( BOT_TITLE, workOpts );
+var logFile = fs.createWriteStream( LOGFILE, { flags: 'a' } );
+
+console.log = function () {
+	logFile.write( util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
+
+process.on( "message", function( m ) {
+
+	if ( m.serv )
+		console.log( "[ " + m.serv + " ] " + m.from + " -> " + m.to + ": " + m.msg );
+} );
+
